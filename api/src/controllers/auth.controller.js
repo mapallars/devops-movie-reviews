3835ce998a690a97aa653a189677fe2bdd788191
mapallars <202194519+mapallars@users.noreply.github.com>
2026-05-04 09:42:39 -0500

    feat: initialize API server with authentication and movie CRUD controllers

--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -1,7 +1,6 @@
 import db from '../models/index.js';
 const User = db.User;
 const Role = db.Role;
-import crypto from 'crypto';
 
 import jwt from 'jsonwebtoken';
 
@@ -69,17 +68,4 @@ export const signin = async (req, res) => {
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
-};
-
-// Vulnerability: Weak Cryptographic Algorithm
-export const legacyLogin = (req, res) => {
-  const { username, password } = req.body;
-  // Semgrep should flag the use of MD5
-  const hash = crypto.createHash('md5').update(password).digest('hex');
-  
-  res.send({ 
-    message: "Legacy login attempt",
-    username: username,
-    hash: hash
-  });
 };
